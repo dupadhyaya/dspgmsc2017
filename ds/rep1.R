13453065cd32193f28ae58b83b38fbbd86baff4e
dupadhyaya <dup1966@gmail.com>
2017-08-27 17:26:08 +0530

    27

---
+++ ds/rep1.R
@@ -0,0 +1,26 @@
+#Repeat  rep
+
+rep(x, ...)
+
+rep.int(x, times)
+
+rep_len(x, length.out)
+
+x = 10
+rep(x)
+rep(x,2)
+rep(x, length.out=3)
+
+?rep
+rep(x, times=1, length.out=NA, each=1)
+x = c(1,2,5)
+rep(x)
+rep(x,2)
+rep(x, length.out=4)  # recycle
+
+# on DF , not meaningful
+x = mtcars[1:5]
+x
+rep(x)
+rep(x,2)
+rep(x, length.out=4)  # recycle
--- packages/resample.R
+++ packages/resample.R
@@ -10,5 +10,33 @@ round(colStdevs(x),2)
 Quantile(x[,1], type = 6)
 
 
+# Extras -----------------------
+
 
 #resample::resample()
+
+
+resample(..., replace = TRUE)
+deal(...)
+
+shuffle(x, replace = FALSE, prob = NULL, groups = NULL,
+        orig.ids = FALSE)
+
+sample(x, size, replace = FALSE, ...)
+
+# S3 method for default
+sample(x, size, replace = FALSE, prob = NULL,
+       groups = NULL, orig.ids = FALSE, ...)
+
+# S3 method for data.frame
+sample(x, size, replace = FALSE, prob = NULL,
+       groups = NULL, orig.ids = TRUE, fixed = names(x), shuffled = c(),
+       invisibly.return = NULL, ...)
+
+# S3 method for matrix
+sample(x, size, replace = FALSE, prob = NULL,
+       groups = NULL, orig.ids = FALSE, ...)
+
+# S3 method for factor
+sample(x, size, replace = FALSE, prob = NULL,
+       groups = NULL, orig.ids = FALSE, drop.unused.levels = FALSE, ...)
