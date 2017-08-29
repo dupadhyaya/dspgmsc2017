50c058778f06851438b41e2cc02aecbdfb90c903
dupadhyaya <dup1966@gmail.com>
2017-08-29 08:47:05 +0530

    a

--- a1/as1.R
+++ a1/as1.R
@@ -9,6 +9,7 @@ length(marks2)
 table(marks2)
 mean(marks2)  # 28.5
 
+#-------------------------
 #Ques-2 : Q2 ---------
 #Find Mean of Various Forms of Data; 
 #Draw a Histogram & Pie Chart of Discrete & Continuous Distribution
@@ -22,77 +23,79 @@ mean(marks2)  # 28.5
 #No of Students : 5 7 9 8
 #A2
 #Obsvn
-marks = c(10 , 12, 13, 14 , 17)
-mean(marks)  # 13.2
+marks2a = c(10 , 12, 13, 14 , 17)
+mean(marks2a)  # 13.2
 
 #Discrete
-marks = c(10 , 12, 13, 14 , 17)
-students = c(5,7,9,8,4)
-sum(students)
-markstotal = marks * students
-markstotal
-sum(markstotal/ sum(students)) #13.06061
+marks2b = c(10 , 12, 13, 14 , 17)
+students2b = c(5,7,9,8,4)
+sum(students2b)
+(markstotal2 = marks2b * students2b)
+
+sum(markstotal2b/ sum(students2b)) #13.06061
 
 #Continuous
-marks3 =c("10-20","20-30","30-40","40-50")
-students3 =c(5,7,9,8)
-df3 = data.frame(marks3, students3)
-df3
-midpt3 =c(15,25,35,45)
-markstotal = students3 * midpt3
-markstotal
-df3a = cbind(df3, markstotal)
-df3a
-mean3 = sum(markstotal)/ sum(students3)
-mean3 #31.89
+marks2c =c("10-20","20-30","30-40","40-50")
+students2c =c(5,7,9,8)
+(df2c = data.frame(marks2c, students2c))
+
+midpt2c =c(15,25,35,45)
+(markstotal2c = students2c * midpt2c)
+
+(df2c = cbind(df2c, markstotal2c))
+
+(mean2c1 = sum(markstotal2c)/ sum(students2c))
+#31.89
 
 
 # Method2
 #class = scan(what='character')
-class3 =c("10-20","20-30","30-40","40-50")
-class3
+class1 = marks2c =c("10-20","20-30","30-40","40-50")
+(students2c =c(5,7,9,8))
 #freq3 = scan()
-freq3 = c(5,7,9,8)
-(class1 = as.numeric(unlist(strsplit(class3,"-"))))  # long method
-(class2 = unique(class1))
+
+(class2 = as.numeric(unlist(strsplit(class1,"-"))))  # long method
+(class3 = unique(class2))
 
 # we need 15, 25, 35, 45
-class2[-length(class2)]
-diff(class2)/2
-(mid3 = class2[-length(class2)] + diff(class2)/2)
-mid3 = as.numeric(mid3)
+class3[-length(class3)]
+diff(class3)/2
+(mid3 = class3[-length(class3)] + diff(class3)/2)
+(mid3 = as.numeric(mid3))
 
-class3 ; mid3 ; freq3
-cbind(class3, mid3, freq3)  #midx3 is also character
-(df3 = data.frame(class3, mid3, freq3))
+class3 ; mid3 ; students2c
+cbind(class1, mid3, students2c)  #mid3 is also character here use DF
+(df2c = data.frame(class1, mid3, students2c))
 
-(df3$midfreq = df3$mid3 * df3$freq3)
-(meanx = sum(df3$midfreq)/ sum(df3$freq3))  # 31.89
+(df2c$midfreq = df2c$mid3 * df2c$students2c)
+(mean2c2 = sum(df2c$midfreq)/ sum(df2c$students2c))  # 31.89
 
 
 # Method3
 # convert into observations
-(x = rep(mid3, freq3))
-mean(x)
+(x3 = rep(mid3, students2c))
+mean(x3)  # 31.89
 
-
+#rm(list=ls())
 # Graphs
 # Discrete
 marks = c(10 , 12, 13, 14 , 17)
 students = c(5,7,9,8,4)
 x3a = rep(marks, students)
 hist(x3a)
+hist(x3a, breaks=3)
 bb= barplot(students, names.arg=marks, ylim=c(0,max(students)+2))
 text(x=bb, y= students+.5, labels = students)
 ?text
 
 # Continuous Data
-hist(x)
-df3
-df3$class3 = factor(df3$class3)
-barplot(df3$freq3, names.arg= df3$class3)
-pie(df3$freq3, labels=df3$class3)
+(x3 = rep(mid3, students2c))
+hist(x3)
+students2c ;class1
+barplot(students2c, names.arg= class1)
+pie(students2c, labels=class1)
 
+# ------------------------------------------------------------
 #Ques-3 :Q3
 #Following are marks of 50 students in a class
 #12 63 61 63 87 65 1 24 67 52 70 55 29 93 30 84 29 27 19 24 32 31 16
@@ -113,4 +116,6 @@ marks.cut = cut(marks, breaks)
 marks.cut
 marks.table = table(marks.cut)
 cbind(marks.table)
+marks
+hist(marks)
 
