8b5242d76856de87c22b9707b4c910ecb1bd75df
dupadhyaya <dup1966@gmail.com>
2017-08-31 20:58:56 +0530

    a

--- stats/cut1.R
+++ stats/cut1.R
@@ -39,24 +39,87 @@ cut(marks, breaks=6)
 x1 = c(0,2,5.1,3,10,14.5, 15,16,20)
 cut(x1,breaks=4, include.lowest=T )  # not good
 #Levels: [-0.02,5] (5,10] (10,15] (15,20]
+
+cut(x1,breaks=4, include.lowest=T)  # not good
+#Levels: [-0.02,5] (5,10] (10,15] (15,20]
+
+
 (bks2 = seq(0,20,5))
+
 cut(x1,breaks=bks2, include.lowest=T )  # not correct
 #Levels: sq[0,5] (5,10] (10,15] (15,20]
 x1 
 # 0.0  2.0  5.1  3.0 10.0 14.5 15.0 16.0 20.0
+
 cut(x1,breaks=bks2, include.lowest=T, right=F )  #  correct
 #Levels: s[0,5) [5,10) [10,15) s[15,20]s
 #s[0,5)[0,5)[5,10) [0,5)[10,15)[10,15)[15,20][15,20] s[15,20]s
 
-cut(x1,breaks=bks2, include.lowest=T, right=T )  #  correct
-#[0,5] (5,10] (10,15] (15,20]
+#understand these steps
 
-cut(x1,breaks=bks2, include.lowest=F, right=T )  #  correct
-#Levels: (0,5]s (5,10] (10,15] (15,20]s sq
-#<NA>(0,5](5,10](0,5](5,10](10,15](10,15](15,20](15,20]s
+#when u do right=TRUE, intervals closed on right. 
+#max value included in last interval, but min value may be lost
+#therefore include.lowest=T required to have it in first interval
+
+x1
+(xc1= cut(x1,breaks=bks2, include.lowest=T, right=T ) ) #  correct
+#s[0,5] (5,10] (10,15] (15,20]s
+#[0,5] [0,5](5,10][0,5] (5,10](10,15](10,15](15,20] (15,20]
+
+# vice-versa works as include.highest now
+(xc2= cut(x1,breaks=bks2, include.lowest=F, right=F ))  # not  correct
+# NA value for max -> correct it
+(xc3= cut(x1,breaks=bks2, include.lowest=T, right=F ))  #  correct
+#Levels: (0,5]s (5,10] (10,15] s[15,20]s 
+#[0,5)[0,5)[5,10)[0,5)[10,15)[10,15)[15,20][15,20][15,20]
+
+table(xc1,useNA='ifany')
+table(xc2, useNA='ifany')
+table(xc3, useNA='ifany')
+?table
 
 
-## generate data for clinical trial example
+# labels to classes
+marks
+(bks = seq(20,80,10))  # 6 intervals
+classint = cut(marks, breaks=bks)
+bks
+length(bks)
+(classint = cut(marks, breaks=bks))  # intervals
+table(classint)
+(classint = cut(marks, breaks=bks, labels=F))  # integers
+table(classint)
+(classint = cut(marks, breaks=bks, labels=LETTERS[1:length(bks)-1]))  # integers
+table(classint)
+?cut
+(classint = cut(marks, breaks=bks, ordered_result = T,
+        labels=LETTERS[1:length(bks)-1]))  # integers
+table(classint)
+plot(classint)
+
+#dig.lab
+## sometimes the default dig.lab is not enough to be avoid confusion:
+aaa <- c(1,2,3,4,5,2,3,4,5,6,7)
+cut(aaa, 3)
+cut(aaa, 3, dig.lab = 4, ordered = TRUE)
+
+## Label construction:
+y <- stats::rnorm(100)
+table(cut(y, breaks = pi/3*(-3:3)))
+table(cut(y, breaks = pi/3*(-3:3), dig.lab = 1))
+table(cut(y, breaks = pi/3*(-3:3), dig.lab = 2))
+table(cut(y, breaks = pi/3*(-3:3), dig.lab = 3))
+table(cut(y, breaks = pi/3*(-3:3), dig.lab = 4))
+
+
+## sometimes the default dig.lab is not enough to be avoid confusion:
+aaa <- c(1,2,3,4,5,2,3,4,5,6,7)
+cut(aaa, 3)
+cut(aaa, 3, dig.lab = 4, ordered = TRUE)
+
+
+
+## generate data for clinical trial example----------
 clinical.trial <-
     data.frame(patient = 1:100,              
                age = rnorm(100, mean = 60, sd = 8),
