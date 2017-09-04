257da3c7dfdf50de942bb537e685cf85d7e46b10
Dhiraj Upadhyaya <dup1966@gmail.com>
2017-09-04 12:43:34 +0530

    a

---
+++ graphs/boxjitter1.R
@@ -0,0 +1,26 @@
+#box plot with jitter
+#http://www.r-graph-gallery.com/96-boxplot-with-jitter/
+#
+plot.new()
+# Create data
+names=c(rep("A", 80) , rep("B", 50) , rep("C", 70))
+value=c( rnorm(80 , mean=10 , sd=9) , rnorm(50 , mean=2 , sd=15) , rnorm(70 , mean=30 , sd=10) )
+data=data.frame(names,value)
+
+# Basic boxplot
+boxplot(data$value ~ data$names , col=terrain.colors(4) )
+
+# Add data points
+mylevels<-levels(data$names)
+levelProportions<-summary(data$names)/nrow(data)
+
+for(i in 1:length(mylevels)){
+  
+  thislevel<-mylevels[i]
+  thisvalues<-data[data$names==thislevel, "value"]
+  
+  # take the x-axis indices and add a jitter, proportional to the N in each level
+  myjitter<-jitter(rep(i, length(thisvalues)), amount=levelProportions[i]/2)
+  points(myjitter, thisvalues, pch=20, col=rgb(0,0,0,.2)) 
+  
+}
