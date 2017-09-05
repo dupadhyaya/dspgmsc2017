c76ee6c2c3f29b6ee866a87721f9e924eb31fddf
dupadhyaya <dup1966@gmail.com>
2017-09-05 08:19:03 +0530

    a

---
+++ graphs/layout1.R
@@ -0,0 +1,46 @@
+# Layout
+# One figure in row 1 and two figures in row 2
+# row 1 is 1/3 the height of row 2
+# column 2 is 1/4 the width of the column 1 
+
+mar = c(5, 4, 4, 2) + 0.1
+attach(mtcars)
+layout(matrix(c(1,1,2,3), 2, 2, byrow = TRUE), 
+       widths=c(3,1), heights=c(1,2))
+hist(wt)
+par('mar')
+graphics.off()
+par(mar=c(1,1,1,1))
+hist(mpg)
+hist(disp)
+
+# 4 figures arranged in 2 rows and 2 columns
+attach(mtcars)
+par(mfrow=c(2,2))
+plot(wt,mpg, main="Scatterplot of wt vs. mpg")
+plot(wt,disp, main="Scatterplot of wt vs disp")
+hist(wt, main="Histogram of wt")
+boxplot(wt, main="Boxplot of wt")
+
+
+# Add boxplots to a scatterplot
+par(fig=c(0,0.8,0,0.8), new=TRUE)
+plot(mtcars$wt, mtcars$mpg, xlab="Car Weight",
+     ylab="Miles Per Gallon")
+par(fig=c(0,0.8,0.55,1), new=TRUE)
+boxplot(mtcars$wt, horizontal=TRUE, axes=FALSE)
+par(fig=c(0.65,1,0,0.8),new=TRUE)
+boxplot(mtcars$mpg, axes=FALSE)
+mtext("Enhanced Scatterplot", side=3, outer=TRUE, line=-3)
+
+?graphics.off()
+par(mar=c(1,1,1,1))
+# Add boxplots to a scatterplot
+par(fig=c(0,0.8,0,0.8), new=TRUE)
+plot(mtcars$wt, mtcars$mpg, xlab="Car Weight",
+     ylab="Miles Per Gallon")
+par(fig=c(0,0.8,0.55,1), new=TRUE)
+boxplot(mtcars$wt, horizontal=TRUE, axes=FALSE)
+par(fig=c(0.65,1,0,0.8),new=TRUE)
+boxplot(mtcars$mpg, axes=FALSE)
+mtext("Enhanced Scatterplot", side=3, outer=TRUE, line=-3)
