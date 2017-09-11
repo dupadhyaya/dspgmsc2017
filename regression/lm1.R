822150575c57d04bd22fe11b91cb317cc0c763ab
dupadhyaya <dup1966@gmail.com>
2017-09-11 08:34:19 +0530

    a

--- regression/lm1.R
+++ regression/lm1.R
@@ -7,6 +7,8 @@ women
 #height	 numeric	 Height (in)
 #weight	 numeric	 Weight (lbs)
 
+# Linear Regreession
+
 attach(women)
 cov(height,weight)
 cor(height,weight)
@@ -19,7 +21,7 @@ abline(weight.lm, col='red')
 
 fitted(weight.lm)
 lines(height, weight, col='blue')
-lines(height, fitted(fit), col='green')
+lines(height, fitted(weight.lm), col='green')
 cbind(height,weight, fitted(weight.lm))
 cbind(height,weight, fitted(weight.lm), weight-fitted(weight.lm), residuals(weight.lm))
 
@@ -30,8 +32,33 @@ new.data = data.frame(height=c(70,75,80))
 predict(weight.lm, newdata = new.data, interval='confidence')
 predict(weight.lm, newdata = new.data, interval='prediction')
 
+
+
+
+# Polynomial Regression X2
+weight.lm2 = lm(weight ~ height + I(height^2), data=women)
+weight.lm2
+summary(weight.lm2)
+plot(women$height, women$weight, xlab='Ht in inch', ylab='Weight in lbs')
+lines(women$height, fitted(weight.lm2), col='purple', lwd=2)
+text(mean(women$height)+5, mean(women$weight), 'Polynomial', col='purple', cex=.8)
+lines(women$height, fitted(weight.lm), col='red', lwd=3)
+text(mean(women$height)-5, mean(women$weight), 'Linear', col='red', cex=.6)
+
+# higher polynomial
+weight.lmn = lm(weight ~ height + I(height^2) + I(height^3), data=women)
+summary(weight.lmn) # max residual errors reduced
+
+# scatter plot for above data
+library(car)
+car::scatterplot(weight ~ height, data=women, spread=F, smoother.args= list(lty=2),
+    pch=19, main='Women age 30-39', xlab='Ht in inch', ylab='Wt in lbs' )
+
 #http://www.dummies.com/programming/r/how-to-predict-new-data-values-with-r/
 
+
+
+# Linear - Polynomial - Multiple Linear
 
 
 # Eruptions
---
+++ strings/strjoin.R
@@ -0,0 +1,12 @@
+# Strings Join
+library(stringr)
+str_c("Letter: ", letters)
+str_c("Letter", letters, sep = ": ")
+str_c(letters, " is for", "...")
+str_c(letters[-26], " comes before ", letters[-1])
+
+str_c(letters, collapse = "")
+str_c(desc(letters), collapse = "")
+str_c(letters, collapse = "")
+
+str_c(letters, collapse = ", ")
