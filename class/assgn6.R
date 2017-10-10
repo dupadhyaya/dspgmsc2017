04d1fde0c4c745764612420beaa785852a8bdeb7
Dhiraj Upadhyaya <dup1966@gmail.com>
2017-10-10 18:12:13 +0530

    a

--- class/assgn6.R
+++ class/assgn6.R
@@ -1,20 +1,44 @@
 #Assignment - 6 
 
 # create a vector of 40 values at equal interval between 10 and 200; use integer values
-(x1 = ceiling (seq(10, 200,length.out = 40)))
+
 
 # use this vector to Create the matrix 5 x 8 : columnwise
-m1<-matrix(x1,nrow=5)
-m1
+
 
 # Return the product of each of the rows
-apply(m1,1,prod)
+
 
 # Return the sum of each of the columns
-apply(m1,2,sum)
+
 
 # Return a new matrix whose entries are those of 'matrix' modulo 10
-apply(m1,c(1,2),function(x) x%%10) 
+
 
 # How many odd nos each column has
-apply(m1, 2, function(x) length(x[x%%2 != 0]))
+
+
+
+# View the data set iris
+# get the mean of the first 4 variables, by species
+
+
+
+# Environment
+# create a new environment
+e <- new.env()
+# two environment variables, a - matrix and b as an array
+e$a <- matrix(1:12,nrow=4)
+e$b <- array(c(1:3*4*2),dim=c(3,4,2))
+# Find the mean of the variables
+
+#check the class of each object
+
+
+
+# create a list with 2 elements 1:20 and 55:100 and mpg and weight coln of mtcars dataset
+
+# Find the sum of the values in each element
+
+# print the values in Vector form
+
