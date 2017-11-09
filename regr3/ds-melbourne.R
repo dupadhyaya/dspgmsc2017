2337d39d0bf227b43138b2e58ad5839976cd3632
Dhiraj Upadhyaya <dup1966@gmail.com>
2017-11-09 10:43:07 +0530

    a

---
+++ regr3/ds-melbourne.R
@@ -0,0 +1,8 @@
+# Melbourne Data Set
+https://www.kaggle.com/anthonypino/melbourne-housing-market
+
+melbourne = read.csv(file.choose())
+str(melbourne)
+summary(melbourne)
+sum(is.na(melbourne))
+colSums(is.na(melbourne))
